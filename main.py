55e3a4595c98d5154d8780e5a1b7e23d9b162869
Pongsakorn2341 <meepanda2341@gmail.com>
2022-10-17 14:57:33 +0700

    Add endpoint /world/city/{name}

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ import csv
 # See https://fastapi.tiangolo.com/tutorial/first-steps/
 from fastapi import FastAPI
 
-# get country data from csv file  
-filename = "world_table_country.csv"
+# get city data from csv file
+filename = "world_table_city.csv"
 with open(filename, "r") as csv_file:
     csv_reader = csv.reader(csv_file)
     headers = next(csv_reader)
-    data_country = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
+    data_country = [{k: v for (k, v) in zip(headers, row)}
+                    for row in csv_reader]
 
 # create an instance of class FastAPI named "app"
 app = FastAPI()
 
 # define function that handles "GET" request with endpoint "/"
+
+
 @app.get("/")
 async def read_root() -> dict:
     return {"message": "Hello there"}
 
+# define function that handles "GET" request with endpoint "/world/city/{name}"
+# "/world/city/{name}" is a "path paramter" endpoint
 
-# define function that handles "GET" request with endpoint "/world"
-# list all countries
-@app.get("/world")
-async def read_countries() -> dict:
-    return {"result": data_country}
+
+@app.get("/world/city/{name}")
+async def read_city(name: str) -> dict:
+    for row in data_city:
+        if row["Name"].lower() == name.lower():
+            return {"result": row}
+    return {"result": {}}
